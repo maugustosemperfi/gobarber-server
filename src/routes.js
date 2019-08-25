c0375e0c83d6ac45f52f2ebebf09cb8fb5fd458a
Marcos Augusto <augustoprofemp@gmail.com>
2019-08-25 08:29:59 -0300

    [FEATURE] Creating user model
    - Creating database connection
    - Changing route to create user

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,12 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
+
+routes.post('/users', async (req, res) => {
+  const user = await User.create(req.body);
+
+  return res.json(user);
+});
 
 export default routes;
